6984405e8ab61968f9e0c76771eedac38479841a
James Brooks <brooksjamesk01@gmail.com>
2022-01-27 18:37:05 -0800

    added additional JavaScript and readjusted some CSS

--- index.js
+++ index.js
@@ -9,27 +9,44 @@ let timeDate = time + ' ' + Fulldate;
 let listColor = document.getElementById("List");
 console.log(timeDate)
 
-let dtAnswer = prompt("Would you like to know the current date and time?");
-
- if (dtAnswer === 'yes') {
-    alert('It is ' + time + ' on ' + Fulldate);
-} else if (dtAnswer === 'no') {
-    alert("It's alright, I'm just a question form");
+function askForTime() {
+        let dtAnswer = prompt("Would you like to know the current date and time?");
+            if (dtAnswer == null) {
+                askForTime();
+            }
+            dtAnswer = dtAnswer.toLowerCase();
+            if (dtAnswer === 'yes') {
+                alert('It is ' + time + ' on ' + Fulldate);
+            } else if (dtAnswer === 'no') {
+                alert("It's alright, I'm just a question form");
+            }
+            else{
+                alert('Please input a Valid input. If answer looks correct, try all lowercase');
+                    if (i<3) {
+                        i++
+                        console.log("Number of Inputs", i)
+                        askForTime();
+                    }
+            }
+        return
 }
-else {
-    alert('Rude');
+function changeListcolor() {
+    if (CurrentS > 40) {
+        listColor.style.color = '#FCA311';
+    }
+    else if (CurrentS > 20) {
+        listColor.style.color = '#DC2F02';
+    }
+    else {
+        listColor.style.color = '#6A040F';
+    }
+    console.log('listColor', listColor)
+    return
 }
 
+askForTime()
+if (i >= 3) {
+    alert('Fine, be that way.')
+}
 alert("Try reloading the page when you're done reading, it might change")
-
-if (CurrentS > 40) {
-    listColor.style.color = '#FCA311';
-}
-else if (CurrentS > 20) {
-    listColor.style.color = '#DC2F02';
-}
-else {
-    listColor.style.color = '#6A040F';
-}
-console.log('listColor', listColor)
- 
+changeListcolor()
